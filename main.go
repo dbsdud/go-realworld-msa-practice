582f6cac6d027c4a886a34f1d5d08bcf2d3b875d
dnsdud <duddl425@gmail.com>
2023-06-15 23:26:09 +0900

    add router & ignore

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"runtime"
-
 	"github.com/gin-gonic/gin"
 )
 
+func v1EndpointHandler(c *gin.Context) {
+	c.String(200, "v1: %s %s", c.Request.Method, c.Request.URL.Path)
+}
 func main() {
 	router := gin.Default()
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
 
-	router.GET("/os", func(c *gin.Context) {
-		c.String(200, runtime.GOOS)
-	})
+	v1 := router.Group("/v1")
+
+	v1.GET("/users", v1EndpointHandler)
+	v1.GET("/users/:userId", v1EndpointHandler)
+	v1.POST("/users", v1EndpointHandler)
+	v1.PUT("/users/:userId", v1EndpointHandler)
+	v1.DELETE("/users/:userId", v1EndpointHandler)
+
 	router.Run(":8080")
 }
